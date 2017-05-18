8590fc59686830f317254453de8fe0a453b55497
Steve Price <spprice@nola.gov>
2017-05-18 17:52:47 -0500

    fix serialization test failures

---
+++ GovDelivery.Tests/SerializationUtilsTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using Xunit;
+using System.Xml.Linq;
+using GovDelivery.Csv.Utils;
+using GovDelivery.Rest.Utils;
+using GovDelivery.Rest.Models.Misc;
+
+namespace GovDelivery.Tests
+{
+    public class SerializationUtilsTests
+    {
+        public SerializationUtilsTests()
+        {
+        }
+
+        [Theory(DisplayName = "Valid test model serializes as expected.")]
+        [MemberData(nameof(SerializeTestArgs))]
+        public async void SerializeTest(string filePath, TestModel testModel)
+        {
+            using (var reader = File.OpenText(filePath))
+            {
+                var xmlString = await reader.ReadToEndAsync();
+
+                var serializedModel = SerializationUtils.ModelToStringContent(testModel);
+
+                var serializedModelString = await serializedModel.ReadAsStringAsync();
+
+                Assert.Equal(xmlString, serializedModelString);
+
+            }
+            
+        }
+
+        [Theory(DisplayName = "Valid test model deserializes as expected.")]
+        [MemberData(nameof(SerializeTestArgs))]
+        public async void DeserializeTest(string filePath, TestModel testModel)
+        {
+            var serializer = new XmlSerializer(typeof(TestModel));
+            using (var reader = File.OpenText(filePath))
+            {
+                var xmlString = await reader.ReadToEndAsync();
+
+                using (var xmlStream = xmlString.ToStream())
+                {
+                    var deserializedModel = (TestModel)serializer.Deserialize(xmlStream);
+
+                    Assert.Equal(testModel.TestArray.Items.Count(), deserializedModel.TestArray.Items.Count());
+                    Assert.Equal(testModel.TestArray.Items.ElementAt(0).TestValue, deserializedModel.TestArray.Items.ElementAt(0).TestValue);
+                    Assert.Equal(testModel.TestArray.Items.ElementAt(1).TestValue, deserializedModel.TestArray.Items.ElementAt(1).TestValue);
+                    Assert.Equal(testModel.TestArray.Items.ElementAt(2).TestValue, deserializedModel.TestArray.Items.ElementAt(2).TestValue);
+
+                    Assert.Equal(testModel.TestBool.Value, deserializedModel.TestBool.Value);
+                    Assert.Equal(testModel.TestInt.Value, deserializedModel.TestInt.Value);
+                    Assert.Equal(testModel.TestString, deserializedModel.TestString);
+                }
+            }
+        }
+
+        public static IEnumerable<object> SerializeTestArgs()
+        {
+            yield return new object[] {
+                @"MockData\mockSerializationData.xml",
+                new TestModel
+                {
+                    TestArray = new TestModel.SerializableTestItemArray
+                    {
+                        Items = new List<TestModel.TestItem>
+                        {
+                            new TestModel.TestItem { TestValue = "foo" },
+                            new TestModel.TestItem { TestValue = "bar" },
+                            new TestModel.TestItem { TestValue = "baz" },
+                        }
+                    },
+                    TestString = "Hello, world!",
+                    TestInt = new SerializableInt { Value = 42 },
+                    TestBool = new SerializableBool { Value = true },
+                },
+            };
+        }
+    }
+
+    
+    [XmlRoot("test-model")]
+    public class TestModel
+    {
+        [XmlElement("test-array")]
+        public SerializableTestItemArray TestArray { get; set; }
+
+        [XmlElement("test-string")]
+        public string TestString { get; set; }
+
+        [XmlElement("test-int")]
+        public SerializableInt TestInt { get; set; }
+
+        [XmlElement("test-bool")]
+        public SerializableBool TestBool { get; set; }
+
+
+        public class TestItem
+        {
+            [XmlElement("test-value")]
+            public string TestValue { get; set; }
+        }
+
+        public class SerializableTestItemArray : BaseSerializableArray<TestItem>
+        {
+            [XmlElement("test-item")]
+            public override List<TestItem> Items { get; set; }
+
+        }
+    }
+
+
+}
---
+++ GovDelivery.Tests/TimeUtilsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using Xunit;
+using System.Xml.Linq;
+using GovDelivery.Csv.Utils;
+using GovDelivery.Rest.Utils;
+using GovDelivery.Rest.Models.Misc;
+
+namespace GovDelivery.Tests
+{
+    public class TimeUtilsTests
+    {
+        public TimeUtilsTests()
+        {
+        }
+
+        [Theory(DisplayName = "Timezones in date strings become UTC offsets")]
+        [InlineData("10/22/2015 03:49 PM CDT")]
+        public void TimeZoneBecomesUtcOffset(string data)
+        {
+            Assert.Equal(TimeUtils.ReplaceTimeZoneWithUtcOffset(data), "10/22/2015 03:49 PM -5");
+        }
+
+        [Theory(DisplayName = "Fixed date string becomes DateTime With Expected Values")]
+        [InlineData("10/22/2015 03:49 PM CDT")]
+        public void FixedDateStringParsesToDateTime(string date)
+        {
+            var dateParts = TimeUtils.ReplaceTimeZoneWithUtcOffset(date).Split(' ').ToList();
+            var dateDay = dateParts.ElementAt(0);
+            var dateTime = dateParts
+                .GetRange(1, dateParts.Count - 1)
+                .Aggregate((memo, next) => $"{memo} {next}");
+
+            Assert.Equal("10/22/2015", dateDay);
+            Assert.Equal("03:49 PM -5", dateTime);
+
+            var parsedDay = DateTime.ParseExact(dateDay, TimeUtils.DATE_FORMAT, CultureInfo.CurrentCulture).ToUniversalTime();
+            Assert.Equal(parsedDay.Year, 2015);
+            Assert.Equal(parsedDay.Month, 10);
+            Assert.Equal(parsedDay.Day, 22);
+
+            var parsedTime = DateTime.ParseExact(dateTime, TimeUtils.TIME_FORMAT, CultureInfo.CurrentCulture).ToUniversalTime();
+            Assert.Equal(20, parsedTime.Hour);
+            Assert.Equal(49, parsedTime.Minute);
+
+            var parsedDate = TimeUtils.DateStringToDateTimeUtc(date);
+            Assert.True(parsedDate.GetType() == typeof(DateTime));
+        }
+
+    }
+
+}
