7a8b5bd50e9816ffd891441c590d32c9503c7887
Steve Price <spprice@nola.gov>
2017-05-22 11:51:33 -0500

    fix failing csv import test

--- GovDelivery.Csv/CsvImporter.cs
+++ GovDelivery.Csv/CsvImporter.cs
@@ -13,36 +13,31 @@ namespace GovDelivery.Csv
     public class CsvImporter : ICsvImporter
     {
 
-        public async Task<string> GetCsvFileContentsAsync(string filePath)
+        public async Task<string> ReadCsvFileContentsAsync(string filePath)
         {
             if (!filePath.ToLowerInvariant().EndsWith(".csv"))
                 throw new Exception("File must be .csv format");
 
-            using (var fileStream = File.Open(filePath, FileMode.Open))
-            using (var reader = new StreamReader(fileStream, true))
+            using (var reader = File.OpenText(filePath))
             {
-                var fileBytes = new byte[fileStream.Length];
-
-                await fileStream.ReadAsync(fileBytes, 0, (int)fileStream.Length);
-
-                return reader.CurrentEncoding.GetString(fileBytes);
+                return await reader.ReadToEndAsync();
             }
         }
 
         public async Task<IEnumerable<IImportSubscriberModel>> ImportSubscribersAsync(string filePath)
         {
-            var fileContents = await GetCsvFileContentsAsync(filePath);
+            var fileContents = await ReadCsvFileContentsAsync(filePath);
 
             var trimmedFileContents = fileContents.Trim();
 
             using (var txtReader = new StringReader(trimmedFileContents))
-            using (var csv = new CsvReader(txtReader))
+            using (var csvReader = new CsvReader(txtReader))
             {
-                csv.Configuration.RegisterClassMap<ImportSubcriberModelMap>();
-                return csv.GetRecords<ImportSubscriberModel>().ToList();
+                csvReader.Configuration.RegisterClassMap<ImportSubcriberModelMap>();
+                return csvReader.GetRecords<ImportSubscriberModel>().ToList();
             }
+
         }
-        
         
     }
 }
