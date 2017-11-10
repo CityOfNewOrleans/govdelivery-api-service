2f0a1883d5d203b8b2f03ab9a5e81876ba4903f2
Stephen Price <spprice@nola.gov>
2017-11-10 16:56:10 -0600

    Add filestream support to csv importer

--- GovDelivery.Csv/CsvImporter.cs
+++ GovDelivery.Csv/CsvImporter.cs
@@ -13,31 +13,36 @@ namespace GovDelivery.Csv
     public class CsvImporter : ICsvImporter
     {
 
-        public async Task<string> ReadCsvFileContentsAsync(string filePath)
+        public async Task<IEnumerable<IImportSubscriberModel>> ImportSubscriberAsync(Stream fileStream) =>
+            ParseFileContents(await ReadStreamContentsAsync(fileStream));
+
+        public async Task<IEnumerable<IImportSubscriberModel>> ImportSubscribersAsync(string filePath) =>
+            ParseFileContents(await ReadCsvFileContentsAsync(filePath));
+
+        protected IEnumerable<IImportSubscriberModel> ParseFileContents(string fileContents) {
+            using (var txtReader = new StringReader(fileContents))
+            using (var csvReader = new CsvReader(txtReader))
+            {
+                csvReader.Configuration.RegisterClassMap<ImportSubcriberModelMap>();
+                return csvReader.GetRecords<ImportSubscriberModel>().ToList();
+            }
+        }
+        
+        protected async Task<string> ReadCsvFileContentsAsync(string filePath)
         {
             if (!filePath.ToLowerInvariant().EndsWith(".csv"))
                 throw new Exception("File must be .csv format");
 
             using (var reader = File.OpenText(filePath))
             {
-                return await reader.ReadToEndAsync();
+                return (await reader.ReadToEndAsync()).Trim();
             }
         }
 
-        public async Task<IEnumerable<IImportSubscriberModel>> ImportSubscribersAsync(string filePath)
-        {
-            var fileContents = await ReadCsvFileContentsAsync(filePath);
-
-            var trimmedFileContents = fileContents.Trim();
-
-            using (var txtReader = new StringReader(trimmedFileContents))
-            using (var csvReader = new CsvReader(txtReader))
-            {
-                csvReader.Configuration.RegisterClassMap<ImportSubcriberModelMap>();
-                return csvReader.GetRecords<ImportSubscriberModel>().ToList();
+        protected async Task<string> ReadStreamContentsAsync(Stream fileStream) {
+            using (var reader = new StreamReader(fileStream)) {
+               return await reader.ReadToEndAsync();
             }
-
         }
-        
     }
 }
